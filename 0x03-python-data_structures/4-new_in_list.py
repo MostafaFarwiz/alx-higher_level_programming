12f03b2963e6558e62821921320a5c46ac67c456
root <root@Farwiz.localdomain>
2023-03-13 23:30:21 +0200

    again

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,8 @@
 def new_in_list(my_list, idx, element):
         length = len(my_list)
 
-            new_list = my_list[:]
+        new_list = my_list[:]
 
-                if 0 <= idx < length:
-                            new_list[idx] = element
-
-                                return (new_list)
+        if 0 <= idx < length:
+                new_list[idx] = element
+        return (new_list)
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,8 @@
 
 def print_matrix_integer(matrix=[[]]):
         for rol in range(len(matrix)):
-                    for col in range(len(matrix[rol])):
-                                    if col != 0:
-                                                        print(" ", end="")
-                                                                    print("{:d}".format(matrix[rol][col]), end='')
-                                                                            print()
+                for col in range(len(matrix[rol])):
+                        if col != 0:
+                                print(" ", end="")
+                        print("{:d}".format(matrix[rol][col]), end='')
+                print()
